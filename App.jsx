5e38176dcffc482490b82e2069c827a19b58d75b
yoshita <104486967+yopy-pixel@users.noreply.github.com>
2022-08-10 15:00:00 +0900

    Create HelloComponent

--- App.jsx
+++ App.jsx
@@ -7,7 +7,8 @@ import Hello from './src/components/Hello';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello>World</Hello>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16 }}>SmallWorld</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
---
+++ src/components/Hello.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { bool, shape, string } from 'prop-types';
+
+function Hello(props) {
+  const { children, bang, style } = props;
+  return (
+    <View>
+      <Text style={[styles.text, style]}>
+        {`Hello  ${children}${bang ? '!' : ''}`}
+      </Text>
+    </View>
+  );
+}
+
+Hello.propType = {
+  children: string.isRequired,
+  bang: bool,
+  style: shape(),
+};
+
+Hello.defaultProps = {
+  bang: false,
+  style: null,
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+});
+
+export default Hello;

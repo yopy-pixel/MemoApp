029d94ff274131b68f0a1eb0d4a2aeb77f695405
yoshita <104486967+yopy-pixel@users.noreply.github.com>
2022-08-25 00:01:36 +0900

    create MemoCreateScreen and MemoEditScreen

--- App.jsx
+++ App.jsx
@@ -2,9 +2,11 @@
 
 import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailsScreen from './src/screens/MemoDetailsScreen';
+//import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }

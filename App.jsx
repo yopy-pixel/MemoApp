3100696d5557ed48d90e5e5c9f904cbc4e8775a2
yoshita <104486967+yopy-pixel@users.noreply.github.com>
2022-08-26 16:44:01 +0900

    create LoginScreen and SignupScreen

--- App.jsx
+++ App.jsx
@@ -4,9 +4,11 @@ import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailsScreen from './src/screens/MemoDetailsScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignupScreen />
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,32 @@
+import { string } from 'prop-types';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Button(props) {
+  const { label } = props;
+  return (
+    <View style={styles.buttonContainer}>
+      <Text style={styles.buttonLabel}>{label}</Text>
+    </View>
+  );
+}
+Button.propTypes = {
+  label: string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#467fd3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#ffffff',
+  },
+
+});
